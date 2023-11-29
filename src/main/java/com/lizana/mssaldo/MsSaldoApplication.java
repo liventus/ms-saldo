431471b158391c1b7bfdb6030032ee8f1f3c5ef7
Your Name <you@example.com>
2023-11-29 17:01:01 -0500

    first commit

---
+++ src/main/java/com/lizana/mssaldo/MsSaldoApplication.java
@@ -0,0 +1,13 @@
+package com.lizana.mssaldo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSaldoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSaldoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lizana/mssaldo/MsSaldoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lizana.mssaldo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSaldoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
